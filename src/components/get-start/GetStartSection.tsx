8b7114d65897dbb67acd2bd2b06858c763d87430
juancodev <montillasanchezjuancarlos@gmail.com>
2023-08-17 18:39:29 -0400

    [⚡ADD] TODO: Footer and networking

--- src/components/get-start/GetStartSection.tsx
+++ src/components/get-start/GetStartSection.tsx
@@ -1,38 +1,25 @@
-import { ButtonPrimary } from "../button/ButtonPrimary";
+import { BtnHero } from "../button/ButtonPrimary";
 
 const GetStartSection = () => {
   return (
     <>
       <section className="relative">
         <div className="bg-slate-800 inset-0">
-          <div className="mx-14 max-w-full px-6">
-            <div className="py-20">
-              <div className="flex justify-between items-center">
-                <div className="text-left mr-16 mb-0">
+          <div className="mx-14 max-w-full px-6 max-md:mx-7 max-md:px-3">
+            <div className="py-20 max-md:py-10">
+              <div className="flex justify-between items-center max-md:flex-col max-md:gap-4">
+                <div className="text-left mr-16 mb-0 max-md:text-center max-md:mr-0">
                   <p className="font-medium text-xl mb-3 text-[#0072ce]">
                     ¿Qué estas esperando?
                   </p>
-                  <h2 className="text-4xl font-bold text-white">
+                  <h2 className="text-4xl font-bold text-white max-md:text-xl max-md:text-center">
                     No le dés más larga a tu educación.
                   </h2>
                 </div>
                 <div className="button shrink-0">
                   <div className="button-container flex justify-start mx-auto">
                     <div>
-                      <ButtonPrimary
-                        title="¡Comienza ahora!"
-                        link="#"
-                        backgroundColor="bg-blue-400"
-                        style={"text-white py-2 px-4 rounded-full mr-4"}
-                      />
-                    </div>
-                    <div>
-                      <ButtonPrimary
-                        title={`Iniciar Sesión`}
-                        link="https://edtecnica.info/"
-                        backgroundColor="bg-slate-700"
-                        style={"text-white py-2 px-4 rounded-full mr-4"}
-                      />
+                      <BtnHero title="Regístrate Gratis" />
                     </div>
                   </div>
                 </div>
